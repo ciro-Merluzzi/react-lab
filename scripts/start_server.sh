42eeddcfed998f13a862cb122f0f44cb64f21e5f
ciro.merluzzi <ciro.merluzzi@summit-bra.com>
2023-04-10 00:20:12 -0300

    .

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#set -x // Debug mode
+set -x // Debug mode
 
 #export PATH="$HOME/.nvm/versions/node/v16.20.0/bin/npm:$PATH"
 #source "$HOME/.nvm/nvm.sh"
 # tá funcionando
 
 #npm install serve
-PATH=$PATH:/root/.nvm/versions/node/v16.20.0/bin/serve
 serve -s /var/www/html/
