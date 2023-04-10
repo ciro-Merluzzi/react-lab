15b26b1d94cb523da3c098757956de7fc00283d7
ciro.merluzzi <ciro.merluzzi@summit-bra.com>
2023-04-10 01:44:33 -0300

    index...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,8 @@ export const Index = () => {
             <h1>Index page</h1>
             <br />
             <h2>Something new here...</h2>
+            <br />
+            <p>One step at a time...</p>
         </>
     )
 }
